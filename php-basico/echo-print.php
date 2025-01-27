07b272d90bbf8776cd8745ddd63d93e02d97e48e
Barbosa <lukasbo9765@gmail.com>
2025-01-27 11:48:50 -0300

    Saída de dados com echo() e print()

---
+++ php-basico/echo-print.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // echo imprime na tela
+        echo 'comando echo<br/>';
+        // print também imprime, so que é uma função
+        // que por sua vez consegue guardar info
+        print 'comando print<hr/>';
+    ?>
+</body>
+</html>
