e9fccac4c4771eb13eb009395e9016aef9648d43
Barbosa <lukasbo9765@gmail.com>
2025-01-27 13:39:27 -0300

    Comentarios

---
+++ php-basico/comentarios.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Comentario para html/css obs: pode ser visto pelo inspecionar -->
+    <?php 
+    // Comentario de uma linha
+    echo 'Comando echo<br/>'
+    # Comentario de uma linha
+
+    /*
+    Comentario de várias linhas
+    */ 
+    ?>
+</body>
+</html>
