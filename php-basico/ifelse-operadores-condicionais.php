6712d50850bc8e295427921463855d6213d29a20
Barbosa <lukasbo9765@gmail.com>
2025-01-27 15:08:09 -0300

    If/else - Praticando com operadores de comparação

---
+++ php-basico/ifelse-operadores-condicionais.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        
+        // == -> igual
+        // === -> idêntico
+        // != ou <> -> diferente
+        // !==
+        // > maior que
+        // < menor que
+        // >= maior ou igual a
+        // <= menor ou igual a
+
+        if(-5 >= 15) {
+            echo 'Verdadeiro';
+        } else { // pode fazer uma condicional sem o else se desejar
+            echo 'Falso';
+        }
+    ?>
+
+</body>
+</html>
