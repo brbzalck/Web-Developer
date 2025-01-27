2303ba92d6ea588663987b8edab11364b903d343
Barbosa <lukasbo9765@gmail.com>
2025-01-27 14:01:25 -0300

    Variáveis - Prática (string, int, float e boolean)

---
+++ php-basico/variaveis.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        // String
+        $nome = 'Lucas Barbosa';
+        // int
+        $idade = 24;
+        // float
+        $peso = 66.5;
+        // bool
+        $fumante_sn = false; // (true = 1) or (false = vazio)
+    ?>
+
+    <h1>Ficha cadastral</h1>
+    <br>
+    <!-- Pode-se usar a tag < ?php echo $var ? > -->
+    <p>Nome: <?= $nome?></p><br>
+    <p>Idade: <?= $idade?></p><br>
+    <p>Peso: <?= $peso?></p><br>
+    <p>Fumante: <?= $fumante_sn?></p>
+
+</body>
+</html>
