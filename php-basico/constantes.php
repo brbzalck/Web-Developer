868bbc2a8e8dd783275a63c665f9a7cf87ba671d
Barbosa <lukasbo9765@gmail.com>
2025-01-27 14:29:35 -0300

    Constantes

---
+++ php-basico/constantes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        define('DB_URL', 'endereco_db_dev');
+        define('DB_USER', 'user_db_dev');
+        define('DB_PASS', 'password_db_dev');
+
+        echo DB_URL .'<br>';
+        echo DB_USER .'<br>';
+        echo DB_PASS .'<br>';
+
+    ?>
+
+</body>
+</html>
