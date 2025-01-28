eeb794c0986316267d0943407260be2fffaee319
Barbosa <lukasbo9765@gmail.com>
2025-01-28 11:43:34 -0300

    Praticando com operadores aritméticos

---
+++ php-basico/aritmeticos.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    
+        $num1 = 50;
+        $num2 = 10;
+    
+        echo "A soma entre $num1 e $num2 é " . ($num1 + $num2).'<br>';
+        echo "A subtração entre $num1 e $num2 é " . ($num1 - $num2).'<br>';
+        echo "A divisão entre $num1 e $num2 é " . ($num1 / $num2).'<br>';
+        echo "A multiplicação entre $num1 e $num2 é " . ($num1 * $num2).'<br>';
+        echo "O resto da divisão entre $num1 e $num2 é " . ($num1 % $num2).'<br>';
+
+    ?>
+</body>
+</html>
