5dd657564629ba768890e4e681e05c54c762ee43
Barbosa <lukasbo9765@gmail.com>
2025-01-30 15:24:32 -0300

    Loops - For

---
+++ php-basico/for.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    
+    
+
+    // for(variavel; condição; incremento)
+    for($x = 1; $x <= 10; $x++) {
+        echo "$x <br>";
+        // pode-se usar break e continue dentro da execução do for
+    }
+    
+    ?>
+</body>
+</html>
