67a2653db393bfbc1483bd894ddbe6abdd0bc026
Barbosa <lukasbo9765@gmail.com>
2025-01-31 15:38:45 -0300

    App Help Desk - Autenticando usuário

--- app_help_desk/valida_login.php
+++ app_help_desk/valida_login.php
@@ -1,15 +1,31 @@
 <?php
 
-    // print_r(($_GET));
-    // echo '<br>';
-    // echo $_GET['email'];
-    // echo '<br>';
-    // echo $_GET['senha'];
+    // variável que verifica se a autenticação foi realizada
+    $usuario_autenticado = false;
 
-    print_r($_POST);
-    echo '<br>';
-    echo $_POST['email'];
-    echo '<br>';
-    echo $_POST['senha'];
+    // usuarios do sistema
+    $usuarios_app = [
+        ['email' => 'adm@teste.com.br', 'senha' => '123456'],
+        ['email' => 'user@teste.com.br', 'senha' => 'abcd'],
+        
+    ];
+
+    // echo '<pre>';
+    //     print_r($usuarios_app);
+    // echo '</pre>';
+
+    foreach($usuarios_app as $user) {
+        if ($user['email'] == $_POST['email'] 
+        and $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autenticado';
+    } else {
+       header('Location: index.php?login=erro');
+    }
+
 
 ?>
