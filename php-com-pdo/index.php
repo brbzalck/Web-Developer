03094b363dd007d142853c0b40cd335eb756b838
Barbosa <lukasbo9765@gmail.com>
2025-02-11 16:01:31 -0300

    Tratando exceptions (PDOException)

--- php-com-pdo/index.php
+++ php-com-pdo/index.php
@@ -8,10 +8,24 @@
     // senha para o PDO conectar ao banco de dados
     $senha = '';
 
-    // instanciando uma conexão de PDO que por sua vez faz a interpretação de $dsn = qual SGBD's, aonde está o banco e qual banco
+    try {
+
+        // instanciando uma conexão de PDO que por sua vez faz a interpretação de $dsn = qual SGBD's, aonde está o banco e qual banco
     // $usuário e $senha para conexão ao banco
     $conexao = new PDO($dsn, $usuario, $senha);
     // o objeto $conexao agora além de representar a conexão, vai conseguir fazer CRUD no banco de dados
+
+    // PDOexception é uma classe para retornar qual tipo de erro ocorreu durante a requisição de conexão
+    } catch(PDOException $e) {
+        // getCode e getMessage são métodos de PDOException
+        echo 'Erro:' . $e->getCode() . '<br>Mensagem:' . $e->getMessage();
+        // Aqui da para registrar em um banco de dados os erros
+    }
+
+
+
+
+    
     
 
 ?>
