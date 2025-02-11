f954afeb7e27345edd79d026e3b8b4576d006f53
Barbosa <lukasbo9765@gmail.com>
2025-02-11 17:13:29 -0300

    PDOStatement Object (Query) com fetchAll

--- php-com-pdo/index.php
+++ php-com-pdo/index.php
@@ -10,32 +10,29 @@
 
     try {
 
-        // instanciando uma conexão de PDO que por sua vez faz a interpretação de $dsn = qual SGBD's, aonde está o banco e qual banco
+    // instanciando uma conexão de PDO que por sua vez faz a interpretação de $dsn = qual SGBD's, aonde está o banco e qual banco
     // $usuário e $senha para conexão ao banco
     $conexao = new PDO($dsn, $usuario, $senha);
-    // o objeto $conexao agora além de representar a conexão, vai conseguir fazer CRUD no banco de dados
-
-    // Criando uma query(consulta) de sql, aonde cria caso n exista tabela com id(a_i) nome email senha
+    
     $query = '
-        create table if not exists tb_usuarios(
-            id int not null primary key auto_increment,
-            nome varchar(50) not null,
-            email varchar(100) not null,
-            senha varchar(32) not null
-        )
+        select * from tb_usuarios
     ';
-
-    // utilizando a instância $conexao e o método exec, ambos de PDO - exec(recebe uma variável do tipo string que contem sql)
-    $retorno = $conexao->exec($query);
-    // o retorno dessa execução retorna quantas linhas da tabela foram alteradas
-    echo $retorno;
     
-    $query = 'delete from tb_usuarios';
-    
-    $retorno = $conexao->exec($query);
-    // 1
-    echo $retorno;
+    // query retorna um PDOStatement - faz o comando e retorna dados para $stmt
+    $stmt = $conexao->query($query);
 
+    echo '<pre>';
+        print_r($stmt);
+    echo '</pre>';
+    
+    // fetchAll é um método de PDOStatement que por sua vez pega o retorno de query e joga em uma array
+    $lista = $stmt->fetchAll();
+    
+    echo '<pre>';
+        print_r($lista);
+    echo '<pre>';
+
+    echo $lista[2]['senha'];
 
     // PDOexception é uma classe para retornar qual tipo de erro ocorreu durante a requisição de conexão
     } catch(PDOException $e) {
