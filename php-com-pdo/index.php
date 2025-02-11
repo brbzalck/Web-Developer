24fd06427d866cd8360cbabb30422e23eb10171a
Barbosa <lukasbo9765@gmail.com>
2025-02-11 16:30:07 -0300

    Executando instruções SQL (Exec)

--- php-com-pdo/index.php
+++ php-com-pdo/index.php
@@ -15,6 +15,28 @@
     $conexao = new PDO($dsn, $usuario, $senha);
     // o objeto $conexao agora além de representar a conexão, vai conseguir fazer CRUD no banco de dados
 
+    // Criando uma query(consulta) de sql, aonde cria caso n exista tabela com id(a_i) nome email senha
+    $query = '
+        create table if not exists tb_usuarios(
+            id int not null primary key auto_increment,
+            nome varchar(50) not null,
+            email varchar(100) not null,
+            senha varchar(32) not null
+        )
+    ';
+
+    // utilizando a instância $conexao e o método exec, ambos de PDO - exec(recebe uma variável do tipo string que contem sql)
+    $retorno = $conexao->exec($query);
+    // o retorno dessa execução retorna quantas linhas da tabela foram alteradas
+    echo $retorno;
+    
+    $query = 'delete from tb_usuarios';
+    
+    $retorno = $conexao->exec($query);
+    // 1
+    echo $retorno;
+
+
     // PDOexception é uma classe para retornar qual tipo de erro ocorreu durante a requisição de conexão
     } catch(PDOException $e) {
         // getCode e getMessage são métodos de PDOException
