a6dd21b75b7aaf0b2f923965c3d70f33923d0e63
Barbosa <lukasbo9765@gmail.com>
2025-02-12 11:41:34 -0300

    Fetch - Retornando apenas um registro

--- php-com-pdo/index.php
+++ php-com-pdo/index.php
@@ -15,7 +15,7 @@
     $conexao = new PDO($dsn, $usuario, $senha);
     
     $query = '
-        select * from tb_usuarios
+        select * from tb_usuarios where id = 8
     ';
     
     // query retorna um PDOStatement - faz o comando e retorna dados para $stmt
@@ -26,13 +26,27 @@
     echo '</pre>';
     
     // fetchAll é um método de PDOStatement que por sua vez pega o retorno de query e joga em uma array
-    $lista = $stmt->fetchAll();
+    // $lista = $stmt->fetchAll(PDO::FETCH_OBJ);
+    // é possível configurar o retorno de fechall
+    // PDO::FETCH_OBJ, PDO::FETCH_NUM, PDO::FETCH_ASSOC
+    // retorna array de objetos, índice por numeros ou associativo
     
+    // fetch é usado para selecionar apenas um registro
+    // ideal quando a query é específica no retorno de apenas uma linha de dados
+    $usuarioFetch = $stmt->fetch(PDO::FETCH_OBJ);
+
+
     echo '<pre>';
-        print_r($lista);
+        print_r($usuarioFetch);
     echo '<pre>';
 
-    echo $lista[2]['senha'];
+    // Como retorna uma única array, pode se acessar o atributo direto da variável
+    echo $usuarioFetch->nome;
+
+    // dependendo do retorno, deve acessar como array de array
+    // echo $lista[2][2];
+    // ou então como array de objetos-> com atributos
+    // echo $lista[2]->nome;
 
     // PDOexception é uma classe para retornar qual tipo de erro ocorreu durante a requisição de conexão
     } catch(PDOException $e) {
