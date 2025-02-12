3223b7df3a761d9a112a3d88489d7575cff72798
Barbosa <lukasbo9765@gmail.com>
2025-02-12 15:30:17 -0300

    App Lista de Tarefas - Criando a classe TarefaService

---
+++ app_lista_tarefas/tarefa.service.php
@@ -0,0 +1,24 @@
+<?php 
+    // CRUD
+    class TarefaService {
+        // CREATE
+        public function inserir() {
+
+        }
+        // READ
+        public function recuperar() {
+
+        }
+        // UPDATE
+        public function atualizar() {
+
+        }
+        // DELETE
+        public function remover() {
+
+        }
+
+
+    }
+
+?>
